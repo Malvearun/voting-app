ca226624fa96046b819b568ca339d6467d4fde61
Malvearun <37962813+Malvearun@users.noreply.github.com>
2020-09-04 13:27:07 +0100

    Update server.js

--- result/server.js
+++ result/server.js
@@ -24,13 +24,13 @@ io.sockets.on('connection', function (socket) {
 });
 
 var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: 'postgres://postgres_user:postgres_password@db/postgres'
 });
 
 async.retry(
   {times: 1000, interval: 1000},
   function(callback) {
-    pg.connect('postgres://postgres_user:postgres_password@db/postgres', function(err, client, done) {
+    pool.connect(function(err, client, done) {
       if (err) {
         console.error("Waiting for db");
       }
